66f151406e55c27e9591305a96995440d4301141
Alex McGivern <alex@aeonofdiscord.com>
2019-06-28 14:03:02 +0100

    off by one

--- data/convert.py
+++ data/convert.py
@@ -1,4 +1,4 @@
 f = open("data.txt", "r", encoding="utf-8")
 o = open("../data.js", "w", encoding="utf-8")
 d = f.read()
-o.write('data="'+d.replace('\r','').replace('\n','\\n')+'"')
+o.write('data="'+d.replace('\r','').replace('\n','\\n')+'"\n')
