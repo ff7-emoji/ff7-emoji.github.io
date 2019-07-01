361271daf7fe677c63073b16d59dfb68bbcd0426
Alex McGivern <alex@aeonofdiscord.com>
2019-07-01 12:32:07 +0100

    added arrow key navigation; cleanup

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-pages = [];
-page = 0;
+"use strict";
+var pages = [];
+var page = 0;
 
 function load(p) {
 	var url = window.location.href.split('?')[0];
@@ -9,8 +10,8 @@ function load(p) {
 
 function open(p) {
 	page = p;
-	main.innerHTML = twemoji.parse(pages[p-1].trim());	
-	pagenumber.innerHTML = p+'/'+(pages.length-1);
+	$("main").innerHTML = twemoji.parse(pages[p-1].trim());
+	$("pagenumber").innerHTML = p+'/'+(pages.length-1);
 }
 
 function next() {
@@ -31,12 +32,24 @@ function begin() {
 	load(1);
 }
 
+function $(id) {
+	return document.getElementById(id);
+}
+
 function run() {
 	pages = data.split(".");
-	btn_begin.onclick = begin;
-	btn_back.onclick = back;	
-	btn_next.onclick = next;
-	btn_end.onclick = end;
+	$("btn_begin").onclick = begin;
+	$("btn_back").onclick = back;
+	$("btn_next").onclick = next;
+	$("btn_end").onclick = end;
+
+	document.body.onkeydown = (e)=>{
+		if(e.key == "ArrowLeft") {
+			back();
+		} else if(e.key == "ArrowRight") {
+			next();
+		}
+	};
 
 	var p = parseInt(location.search.substr(1));
 	if(isNaN(p))
